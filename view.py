818f9cacba169ecd0517d7f292f30adc301422d2
aposgial <apostolosgls@mail.com>
2023-12-14 03:21:14 +0200

    main window geometry & title setted

--- view.py
+++ view.py
@@ -1,9 +1,10 @@
 from tkinter import *
 from tkinter import ttk
 import customtkinter
+from settings import *
 
 class View(customtkinter.CTk):
     def __init__(self, *args, **kwargs) -> None:
         super().__init__(*args, **kwargs)
         self.geometry("1200x800")
-        self.title("Lead generator")
+        self.title(MAIN_WINDOW_TITLE)
