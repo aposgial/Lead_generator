fdc4cf7b1ff0d4bab79b0262fef461611192ada6
aposgial <apostolosgls@mail.com>
2023-12-14 17:08:53 +0200

    model read json file implacement

--- model.py
+++ model.py
@@ -1,3 +1,12 @@
+import json
 
 class Model():
-    pass
+    def __init__(self) -> None:
+        self.paces_file = "places_search.json"
+        self.place_file = "place_details.json"
+
+
+    def read_json_file(self, file_name):
+        with open(file_name, 'r') as file:
+            data = json.load(file)
+        return data
