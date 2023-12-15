ad15337b420d6797d11a133ebfeb025b71029741
aposgial <apostolosgls@mail.com>
2023-12-15 02:08:46 +0200

    Google_APIs class seperated to google_API_client file

---
+++ APIs/google_API_client.py
@@ -0,0 +1,11 @@
+import json
+
+class Google_APIs():
+    def __init__(self) -> None:
+        self.places_file = "places_search.json"
+        self.place_file = "place_details.json"
+
+    def read_json_file(self, file_name):
+        with open(file_name, 'r') as file:
+            data = json.load(file)
+        return data
--- APIs/google_APIs.py
+++ APIs/google_APIs.py
@@ -1,15 +1,4 @@
-import json
-
-class Google_APIs():
-    def __init__(self) -> None:
-        self.places_file = "places_search.json"
-        self.place_file = "place_details.json"
-
-    def read_json_file(self, file_name):
-        with open(file_name, 'r') as file:
-            data = json.load(file)
-        return data
-
+from .google_API_client import Google_APIs
 
 class Google_Places_API(Google_APIs):
     def __init__(self) -> None:
